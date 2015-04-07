a8f20f12b8eab2d1e652d076ffb148fcc160bd5f
Antipov Mikhail <orangelemon2007@yandex.ru>
2015-04-07 21:05:51 +0300

    Реализован класс фигуры трапеция.

---
+++ src/Trapeze.java
@@ -0,0 +1,26 @@
+public class Trapeze implements Shape {
+    float a;
+    float b;
+    float c;
+    float d;
+
+
+    public Trapeze(float d, float c, float b, float a) {
+        this.d = d;
+        this.c = c;
+        this.b = b;
+        this.a = a;
+    }
+
+
+    @Override
+    public double perimeter() {
+        return a+b+c+d;
+    }
+
+    @Override
+    public double area() {
+        double p=(a+b+c+d)/2;
+        return (a+b)*Math.sqrt((p-a)*(p-b)*(p-a-c)*(p-a-d))/(4*Math.abs(a-b));
+    }
+}
