4cba1ad0f91e9d7fb3ce47c66aaf0cf7d94ebfda
Alexander Kaznacheev <aakzn@mail.ru>
2015-04-07 20:32:02 +0300

    #5_Rectangle_class Реализован класс фигуры прямоугольник

---
+++ src/Rectangle.java
@@ -0,0 +1,23 @@
+/**
+ * Created by IKZN on 07.04.2015.
+ */
+public class Rectangle implements Shape{
+    int a;
+    int b;
+
+    public Rectangle(int a,int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public double perimeter() {
+
+        return (a+b)*2;
+    }
+
+    @Override
+    public double area() {
+        return a*b;
+    }
+}
