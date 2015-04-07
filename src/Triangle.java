0a61102a37878dd883b5113127baac8d2295937a
Alexander Kaznacheev <aakzn@mail.ru>
2015-04-07 20:26:13 +0300

    #5_Thiangle_class Реализован класс фигуры треугольник

---
+++ src/Triangle.java
@@ -0,0 +1,66 @@
+/**
+ * Created by IKZN on 07.04.2015.
+ */
+public class Triangle implements Shape{
+    //стороны
+    int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    int b;
+    int c;
+   /* //углы
+    int alpha;
+    int beta;
+    int gamma;*/
+
+
+  /*  public Triangle(int a,int b,int c,int alpha,int beta,int gamma) {
+        this.a=a;
+        this.b=b;
+        this.c=c;
+        this.alpha = alpha;
+        this.beta = beta;
+        this.gamma = gamma;
+    }*/
+
+    public Triangle(int a,int b,int c) {
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+
+    @Override
+    public double area() {
+        double p=(a+b+c)/2;
+        double S=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+        return S;
+    }
+
+    @Override
+    public double perimeter() {
+        return a+b+c;
+    }
+}
