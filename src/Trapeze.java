9b52b06fd26a7e539a0380434bc451df8a661c6c
Antipov Mikhail <orangelemon2007@yandex.ru>
2015-04-08 09:56:34 +0300

    Реализована класс фигуры трапеция +  доделан toString().

--- src/Trapeze.java
+++ src/Trapeze.java
@@ -26,6 +26,6 @@ public class Trapeze implements Shape {
 
     @Override
     public String ToString() {
-        return super.toString() + "\t" + "area=" + this.area() + "perimeter=" + this.perimeter();
+        return super.toString() + "\t" + "area trapeze=" + this.area() + "perimeter trapeze=" + this.perimeter();
     }
 }
