0b53492cfc5662aa0df9abd67fed1d3fbff897e2
vl2z <vl2z@mail.ru>
2015-04-08 11:03:03 +0300

    little changes applied2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
         Circle circle = new Circle(5);
-        System.out.println(circle.toString());
+        System.out.println(circle);
 
         Rectangle rectangle = new Rectangle(3, 9);
-        System.out.println(rectangle.toString());
+        System.out.println(rectangle);
 
         Parallelogram parallelogram = new Parallelogram(4, 9, 0, 3);
-        System.out.println(parallelogram.toString());
+        System.out.println(parallelogram);
 
         Rhombus rhombus = new Rhombus(7, 30, 0);
-        System.out.println(rhombus.toString());
+        System.out.println(rhombus);
 
         Trapeze trapeze = new Trapeze(3, 5, 2, 7);
-        System.out.println(trapeze.toString());
+        System.out.println(trapeze);
 
         Triangle triangle = new Triangle(2, 3, 4);
-        System.out.println(triangle.toString());
+        System.out.println(triangle);
     }
 }
